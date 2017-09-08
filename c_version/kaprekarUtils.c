f73147a09c6ec5f97cbb85e1960d1707302ffff9
Alex Richardson <alexrichardsonschoolgmail.com>
2017-09-07 23:07:06 -0500

    Changed modulus to start at 1 rather than 0. Avoids a divide-by-zero error.

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -31,7 +31,7 @@ int isKaprekar(int n) {
   int i;
   long square = n * (long) n;
   int numDigits = (int) log10(square) + 1;
-  long modulus = 0;
+  long modulus = 1;
   long first, second;
   for(i=1; i<=numDigits; i++) {
     modulus *= 10;
