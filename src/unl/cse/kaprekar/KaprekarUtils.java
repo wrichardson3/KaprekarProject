32542c3e06a0984c28b2b11b3a6ad583a4f19b90
Alex Richardson <alexrichardsonschoolgmail.com>
2017-09-07 23:56:00 -0500

    The algorithm wasn't eliminated powers of 10 correctly. Fixed.

--- src/unl/cse/kaprekar/KaprekarUtils.java
+++ src/unl/cse/kaprekar/KaprekarUtils.java
@@ -37,7 +37,7 @@ public class KaprekarUtils {
 			modulus *= 10;
 			first = square / modulus;
 			second = square % modulus;
-			if(first > 0 &&
+			if(second > 0 &&
 			   first + second == n) {
 				return true;
 			}
