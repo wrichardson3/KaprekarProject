aae5cdf42fd310dbb3d0211ebfa5d7ec9a779a7e
Alex Richardson <alexrichardsonschoolgmail.com>
2017-09-07 23:47:34 -0500

    Changed a '=' to a '=='.

--- src/unl/cse/kaprekar/KaprekarUtils.java
+++ src/unl/cse/kaprekar/KaprekarUtils.java
@@ -38,7 +38,7 @@ public class KaprekarUtils {
 			first = square / modulus;
 			second = square % modulus;
 			if(first > 0 &&
-			   first + second = n) {
+			   first + second == n) {
 				return true;
 			}
 		}
